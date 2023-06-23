3db75a36be3fc02867534c7a992684bc1458cf67
bakiscofield <bakitchassei@gmail.com>
2023-06-23 13:30:04 +0000

    premere modification

--- source/app/Models/Consultant.php
+++ source/app/Models/Consultant.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -43,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function user():BelongsTo
+    {
+        return $this-> belongsTo(User::class);
+    }
+
 }
